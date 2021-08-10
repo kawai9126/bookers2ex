46ec8df6103dc6f2b297458484af09aa622f4236
kawai9126 <iroha.9126@gmail.com>
2021-08-10 11:53:35 +0000

    add _new _info user.index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
     def index
         @users = User.all
+        @user = current_user
+        @book = Book.new
     end
     
     def show
@@ -24,5 +26,8 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:name,:introduction,:profile_image)
+    end
+    def book_params
+        params.require(:book).permit(:image,:title,:body)
     end
 end
