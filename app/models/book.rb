9c6b2186d97a1e5e4c1cf823a60deb5c7a13001a
kawai9126 <iroha.9126@gmail.com>
2021-08-12 12:34:24 +0000

    add favorite controller model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
     attachment :image
     
     belongs_to :user
+    has_many :favorites, dependent: :destroy
+    has_many :post_comments, dependent: :destroy
+    
+    def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
          attachment :profile_image
          
          has_many :books
+         has_many :favorites, dependent: :destroy
+         has_many :post_comments, dependent: :destroy
 end
