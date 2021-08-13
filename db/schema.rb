eab56cfef0cdf44e1b60c4451240b2cc057028a9
kawai9126 <iroha.9126@gmail.com>
2021-08-13 13:37:56 +0000

    follow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_122013) do
+ActiveRecord::Schema.define(version: 2021_08_13_133643) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_08_12_122013) do
     t.integer "user_id"
     t.integer "book_id"
     t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "followed_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
